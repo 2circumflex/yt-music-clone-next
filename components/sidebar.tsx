6c07e7f3f4b08bf96cc6320356968ad546fd2238
Roy <2circumflex@gmail.com>
2024-10-07 17:09:01 +0900

    5.2 Sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+import Logo from "./elements/logo";
+import Navigator from "./elements/navigator";
+
+export const Sidebar = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex flex-row h-full">
+      <nav className="w-[240px] border-r-[1px] border-neutral-600">
+        <div className="p-[24px]">
+          <Logo />
+        </div>
+        <div>
+          <Navigator />
+        </div>
+      </nav>
+      <div className="flex-1">{children}</div>
+    </div>
+  );
+};
