07a53d3bc1d1534877f5aa98713b469b24910bfe
Roy <2circumflex@gmail.com>
2024-10-08 00:21:18 +0900

    6.6 Header - search

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React, { ReactNode } from "react";
 import UserIcon from "./user-icon";
 import PagePadding from "./page-padding";
+import { FaChromecast } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 
 const Header = ({ children }: { children: ReactNode }) => {
   return (
@@ -20,8 +22,22 @@ const Header = ({ children }: { children: ReactNode }) => {
       </section>
       <section className="sticky">
         <PagePadding>
-          searchSection
-          <UserIcon />
+          <div className="h-[64px] flex flex-row justify-between items-center">
+            <article className="h-[42px] min-w-[480px] flex flex-row items-center bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px]">
+              <div>
+                <FiSearch size={24} />
+              </div>
+              <input
+                className="h-full w-full bg-transparent"
+                placeholder="노래, 앨범, 아티스트, 팟캐스트 검색"
+                type="text"
+              />
+            </article>
+            <article className="flex flex-row gap-4 items-center">
+              <FaChromecast size={26} />
+              <UserIcon />
+            </article>
+          </div>
         </PagePadding>
       </section>
       <section className="relative">{children}</section>
