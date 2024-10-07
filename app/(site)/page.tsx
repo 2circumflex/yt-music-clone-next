4588ba658deea7511234970412b18fc1484df44f
Roy <2circumflex@gmail.com>
2024-10-07 16:15:15 +0900

    4.6 rootLayout 에서 피해야 할 것

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,10 @@
+import { sleep } from "@/lib/utils";
+
 export default async function Home() {
+  console.log("before Home sleep ...");
+  await sleep(4000);
+  console.log("after Home sleep ...");
+  // Layout에서는 오래 걸리는 작업을 하면 안됨
+
   return <div>HomePage</div>;
 }
