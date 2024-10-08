f16bb910c662b5472088d6c2aa576009157a3703
Roy <2circumflex@gmail.com>
2024-10-08 17:05:43 +0900

    7.2 zustand home 카테고리

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,11 @@
+import Category from "./components/category";
+
 export default async function Home() {
   return (
     <div className="min-h-[600px]">
-      {" "}
-      HomePage
-      <div className="h-[500px] bg-neutral-700">HomePage</div>
-      <div className="h-[500px] bg-neutral-700">HomePage</div>
-      <div className="h-[500px] bg-neutral-700">HomePage</div>
-      <div className="h-[500px] bg-neutral-700">HomePage</div>
+      <div className="mt-9">
+        <Category />
+      </div>
     </div>
   );
 }
