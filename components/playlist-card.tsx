718c0daffb8d07727b55d393db7999f8c29e5775
Roy <2circumflex@gmail.com>
2024-10-09 17:56:21 +0900

    7.6 PlayListCard - 2

--- components/playlist-card.tsx
+++ components/playlist-card.tsx
@@ -5,6 +5,9 @@ import { Playlist, Song } from "@/types";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { MdMoreVert } from "react-icons/md";
+import { FiPlay } from "react-icons/fi";
+import IconButton from "./elements/icon-button";
 
 const PlaylistCard = ({ playlist }: { playlist: Playlist }) => {
   const router = useRouter();
@@ -17,8 +20,16 @@ const PlaylistCard = ({ playlist }: { playlist: Playlist }) => {
     router.push(`/playlist?list=${id}`);
   };
 
+  const onClickPlay = () => {
+    // TODO: 플레이
+  };
+
+  const onClickMore = () => {
+    // TODO: 더보기
+  };
+
   return (
-    <article className="h-[240px] cursor-pointer">
+    <article className="h-[240px] cursor-pointer group">
       <section onClick={onClickCard} className="relative h-[136px]">
         <Image
           src={imgSrc}
@@ -26,6 +37,20 @@ const PlaylistCard = ({ playlist }: { playlist: Playlist }) => {
           alt="thumbnail"
           className="object-cover"
         />
+        <div className="hidden relative group-hover:block bg-gradient-to-b from-[rgba(0,0,0,0.7)] top-0 w-full h-[136px]">
+          <div className="absolute top-2 right-4">
+            <IconButton
+              onClickIcon={onClickMore}
+              icon={<MdMoreVert size={20} />}
+            />
+          </div>
+          <div
+            onClick={onClickPlay}
+            className="absolute bottom-4 right-4 flex items-center justify-center transform-gpu transition-transform hover:scale-110 bg-[rgba(0,0,0,0.7)] w-[45px] h-[45px] rounded-full hover:bg-[rgba(0,0,0,0.9)] pl-[1.5px]"
+          >
+            <FiPlay size={22} color="red" />
+          </div>
+        </div>
       </section>
       <section className="mt-2">
         <div>{playlistName}</div>
