2e90a2fd8899ff8a2d67e1330a70e82625160a2a
Roy <2circumflex@gmail.com>
2024-10-09 19:14:54 +0900

    8.1 Category

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,7 +1,14 @@
+import PagePadding from "@/components/page-padding";
 import React from "react";
+import Category from "./components/category";
 
 const page = () => {
-  return <div>explore</div>;
+  return (
+    <PagePadding>
+      <div className="mt-4"></div>
+      <Category />
+    </PagePadding>
+  );
 };
 
 export default page;
