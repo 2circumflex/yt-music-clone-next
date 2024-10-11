4dff875380f3d17dd8bfbb09fc8f62979c9f68ca
Roy <2circumflex@gmail.com>
2024-10-11 18:34:47 +0900

    9.2 dropdownmenu-2

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,7 +1,13 @@
+import PagePadding from "@/components/page-padding";
 import React from "react";
+import Category from "./components/category";
 
 const page = () => {
-  return <div>library</div>;
+  return (
+    <PagePadding>
+      <Category />
+    </PagePadding>
+  );
 };
 
 export default page;
