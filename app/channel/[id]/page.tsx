ed974c1db4c06dc7c324162a23ae44cec7971577
Roy <2circumflex@gmail.com>
2024-10-12 19:59:47 +0900

    11.2 SSR - getPlaylistById

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,3 +1,9 @@
+import HeaderBgChanger from "@/components/header-bg-changer";
+import PagePadding from "@/components/page-padding";
+import { getChannelById } from "@/lib/dummy-data";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { Song } from "@/types";
+import { permanentRedirect } from "next/navigation";
 import React from "react";
 
 interface ChannelPageProps {
@@ -6,8 +12,22 @@ interface ChannelPageProps {
   };
 }
 
-const page = (props: ChannelPageProps) => {
-  return <div>channel/{props.params.id}</div>;
+const page = async (props: ChannelPageProps) => {
+  const channel = await getChannelById(Number(props.params.id));
+
+  if (!channel) {
+    permanentRedirect("/");
+  }
+
+  const imageSrc: string = (getRandomElementFromArray(channel.songList) as Song)
+    .imageSrc;
+
+  return (
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      <div>channel/{props.params.id}</div>
+    </PagePadding>
+  );
 };
 
 export default page;
