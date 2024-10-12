bab54a3b008539ee8fd7847bbbd6b023d117985b
Roy <2circumflex@gmail.com>
2024-10-12 18:27:46 +0900

    10.2 HeaderBgChanger

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,4 +1,6 @@
+import HeaderBgChanger from "@/components/header-bg-changer";
 import { getPlaylistById } from "@/lib/dummy-data";
+import { getRandomElementFromArray } from "@/lib/utils";
 import { permanentRedirect } from "next/navigation";
 import React from "react";
 
@@ -10,11 +12,21 @@ interface PlaylistPageProps {
 
 const page = async (props: PlaylistPageProps) => {
   const playlist = await getPlaylistById(Number(props.searchParams.list));
+
   if (!playlist) {
     permanentRedirect("/");
   }
 
-  return <div>playlist {props.searchParams.list}</div>;
+  const imageSrc: string = getRandomElementFromArray(
+    playlist.songList
+  )?.imageSrc;
+
+  return (
+    <div>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      playlist {props.searchParams.list}
+    </div>
+  );
 };
 
 export default page;
