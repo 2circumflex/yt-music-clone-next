4ee56edb7c56036978e10a0a1057fb87b4ed3b00
Roy <2circumflex@gmail.com>
2024-10-13 15:36:55 +0900

    12.8 useAudio 사용하기

--- components/player/player-content.tsx
+++ components/player/player-content.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { PlayerSlider } from "../ui/player-slider";
+import { useAudio } from "react-use";
 
 const PlayerContent = () => {
+  const [audio, state, controls, ref] = useAudio({
+    src: "",
+    autoPlay: true,
+  });
+
   return (
     <div className="h-full w-full relative">
       <div className="absolute top-[-16px] w-full">
